4dc6cb2c996af3a3c9d6cd7819768a8abb2808f9
김병윤 <byung-yoon@gimbyeong-yun-ui-MacBookPro.local>
2023-03-13 15:19:01 +0900

    [ADD] 유튜브 설명 영상 삽입, 메인 페이지 디자인 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import styles from "../styles/Home.module.css";
 import { useEffect, useState } from "react";
 import db from "../../firebase/firebase.config";
-import {
-  arrayUnion,
-  collection,
-  CollectionReference,
-  doc,
-  DocumentData,
-  getDoc,
-  setDoc,
-  updateDoc,
-} from "firebase/firestore";
-import { useRouter, NextRouter } from "next/router";
+import { arrayUnion, doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import { setRoomCode } from "store/slices/roomSlice";
 import { AnyAction, Dispatch } from "@reduxjs/toolkit";
 import { createUser, setNickname, setUserId } from "store/slices/userSlice";
@@ -68,13 +58,11 @@ export default function Home() {
       const roomCode = Cookies.get("roomCode");
       const nickname = Cookies.get("nickname");
       const userId = Cookies.get("userId");
-      console.log(`userId:`, userId);
       if (!roomCode) return;
       const docRef = doc(db, "games", roomCode);
       (async () => {
         const data = (await getDoc(docRef)).data();
         if (data) {
-          console.log(`data:`, data);
           dispatch(setRoomCode(roomCode));
           dispatch(setNickname(nickname));
           dispatch(setUserId(userId));
@@ -89,58 +77,75 @@ export default function Home() {
     <Room />
   ) : (
     <main className={styles.main}>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <h1>노 터치 크라켄 온라인에 오신 것을 환영합니다!</h1>
-      <form>
-        <div className="mb-3">
-          <label htmlFor="nicknameInput" className="form-label">
-            닉네임(필수)
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="nickNameInput"
-            onChange={(e) => setUsername(e.target.value)}
-          />
+      {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
+      <h1 className={styles.welcome}>
+        노 터치 크라켄 온라인에 오신 것을 환영합니다!
+      </h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/ckd5Au7lryE?start=12"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            ></iframe>
+          </div>
+          <div className="col-sm">
+            <form>
+              <div className="mb-3">
+                <label htmlFor="nicknameInput" className="form-label">
+                  닉네임(필수)
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="nickNameInput"
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="roomCodeInput" className="form-label">
+                  입장 코드
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="roomCodeInput"
+                  onChange={(e) => setEntryCode(e.target.value)}
+                  maxLength={7}
+                />
+              </div>
+              <div id="enterCodeDescription" className="form-text">
+                방 생성 시에는 입장 코드를 입력하지 말아주세요.
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary"
+                disabled={!username || !!entryCode}
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleCreate(username, dispatch);
+                }}
+              >
+                방 생성
+              </button>
+              <button
+                type="submit"
+                className="btn btn-success"
+                disabled={!username || !/^[A-Z]{7}$/.test(entryCode)}
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleJoin(entryCode, username, dispatch);
+                }}
+              >
+                방 입장
+              </button>
+            </form>
+          </div>
         </div>
-        <div className="mb-3">
-          <label htmlFor="roomCodeInput" className="form-label">
-            입장 코드
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="roomCodeInput"
-            onChange={(e) => setEntryCode(e.target.value)}
-            maxLength={7}
-          />
-        </div>
-        <div id="enterCodeDescription" className="form-text">
-          방 생성 시에는 입장 코드를 입력하지 말아주세요.
-        </div>
-        <button
-          type="submit"
-          className="btn btn-primary"
-          disabled={!username || !!entryCode}
-          onClick={(e) => {
-            e.preventDefault();
-            handleCreate(username, dispatch);
-          }}
-        >
-          방 생성
-        </button>
-        <button
-          type="submit"
-          className="btn btn-success"
-          disabled={!username || !/^[A-Z]{7}$/.test(entryCode)}
-          onClick={(e) => {
-            e.preventDefault();
-            handleJoin(entryCode, username, dispatch);
-          }}
-        >
-          방 입장
-        </button>
-      </form>
+      </div>
     </main>
   );
 }
